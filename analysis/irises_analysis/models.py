5f6d7245fa7d63796984e55ecef5550d603220be
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-01-14 17:25:46 +0100

    add iris DB model

--- analysis/irises_analysis/models.py
+++ analysis/irises_analysis/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Iris(models.Model):
+    id = models.AutoField(primary_key=True)
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    iris_class = models.IntegerField()
