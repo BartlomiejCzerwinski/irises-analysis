706c7e1c9e6d83ea41b7ce7ee8878d1a7ff76075
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-01-14 17:18:02 +0100

    fix add iris form

--- analysis/irises_analysis/views.py
+++ analysis/irises_analysis/views.py
@@ -8,4 +8,11 @@ def index(request):
 
 def add(request):
     form = form_add.Form_add()
+    if request.method == "POST":
+        new_iris_form = form_add.Form_add(request.POST)
+        if new_iris_form.is_valid():
+            data = new_iris_form.cleaned_data
+            return HttpResponse(data['sepal_length'])
+        else:
+            return HttpResponse(new_iris_form)
     return render(request, "irises_analysis/add.html", {'form': form})
