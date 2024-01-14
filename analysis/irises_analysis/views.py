8fedff459bee06c0c20bbbc478693a9b9e163bf6
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-01-14 18:57:14 +0100

    add api on /api/data

--- analysis/irises_analysis/views.py
+++ analysis/irises_analysis/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.serializers import serialize
+import requests
 from . import form_add
+from . import models
 
 # Create your views here.
 def index(request):
+    api_get_data_url = 'http://127.0.0.1:8000/api/data'
+    response = requests.get(api_get_data_url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        return render(request, "irises_analysis/index.html", {'data': data})
     return render(request, "irises_analysis/index.html")
 
 def add(request):
@@ -12,7 +21,21 @@ def add(request):
         new_iris_form = form_add.Form_add(request.POST)
         if new_iris_form.is_valid():
             data = new_iris_form.cleaned_data
-            return HttpResponse(data['sepal_length'])
+            iris = models.Iris(sepal_length=data["sepal_length"],
+                                sepal_width=data["sepal_width"],
+                                petal_length=data["petal_length"],
+                                petal_width=data["petal_width"],
+                                iris_class=data["iris_class"])
+            iris.save()
+            return HttpResponse(data['iris_class'])
         else:
             return HttpResponse(new_iris_form)
     return render(request, "irises_analysis/add.html", {'form': form})
+
+def data(request):
+    if request.method == "GET":
+        queryset = models.Iris.objects.all()
+        data = serialize('json', queryset)
+        return HttpResponse(data)
+    if request.method == "POST":
+        return HttpResponse("POST")
