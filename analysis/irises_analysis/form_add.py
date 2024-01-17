5994c4f32f981e347b7f8d59c056d64e6a99d5d7
BartlomiejCzerwinski <242372@edu.p.lodz.pl>
2024-01-17 11:18:33 +0100

    add form validator

--- analysis/irises_analysis/form_add.py
+++ analysis/irises_analysis/form_add.py
@@ -1,15 +1,19 @@
 from django import forms
 
+def is_positive_validator(value):
+    if value <= 0:
+        raise forms.ValidationError("This field must be greater than 0")
+
 class Form_add(forms.Form):
     CLASS_CHOICES = [
             (1, 'Setosa'),
             (2, 'Versicolour'),
             (3, 'Virginica'),
         ]
-    sepal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal length'}), label='')
-    sepal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal width'}), label='')
-    petal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal length'}), label='')
-    petal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal width'}), label='')
+    sepal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal length'}), label='', validators=[is_positive_validator])
+    sepal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal width'}), label='', validators=[is_positive_validator])
+    petal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal length'}), label='', validators=[is_positive_validator])
+    petal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal width'}), label='', validators=[is_positive_validator])
     iris_class = forms.ChoiceField(
         label="Class",
         choices=CLASS_CHOICES,
@@ -17,7 +21,7 @@ class Form_add(forms.Form):
     )
 
 class Form_predict(forms.Form):
-    sepal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal length'}), label='')
-    sepal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal width'}), label='')
-    petal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal length'}), label='')
-    petal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal width'}), label='')
+    sepal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal length'}), label='', validators=[is_positive_validator])
+    sepal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'sepal width'}), label='', validators=[is_positive_validator])
+    petal_length = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal length'}), label='', validators=[is_positive_validator])
+    petal_width = forms.FloatField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'petal width'}), label='', validators=[is_positive_validator])
